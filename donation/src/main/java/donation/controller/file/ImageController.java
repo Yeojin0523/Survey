33454226ddd51f485363fe229a3a63ce8712fdb7
Yeojin0523 <d02jyjeong@naver.com>
2024-08-24 21:09:17 +0900

    의존성 주입

--- donation/src/main/java/donation/controller/file/ImageController.java
+++ donation/src/main/java/donation/controller/file/ImageController.java
@@ -2,13 +2,21 @@ package donation.controller.file;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import donation.dto.ImageDto;
 
 @RestController
 public class ImageController {
-	private final ImageService imageService = new ImageService();
+	
+	
+	ImageService imageService;
+	
+	@Autowired
+	public ImageController(ImageService imageService) {
+		this.imageService = imageService;
+	}
 	
 	public List<ImageDto> listing(){
 		return imageService.getAllImages();
