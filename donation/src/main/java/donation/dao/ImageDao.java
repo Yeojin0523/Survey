5b077cfb8703f93b427b8936a9e082c5148354bf
Yeojin0523 <d02jyjeong@naver.com>
2024-08-24 20:08:50 +0900

    ImageDao 작성

--- donation/src/main/java/donation/dao/ImageDao.java
+++ donation/src/main/java/donation/dao/ImageDao.java
@@ -1,9 +1,38 @@
 package donation.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import donation.dto.ImageDto;
 
-public interface ImageDao extends JpaRepository<ImageDto, Long>{
-	List<ImageDto> findAll();
+public class ImageDao{
+	// DB 연결 parameters
+	private String jdbcURL = "jdbc:donation_database_url";
+	private String jdbcUsername ="donation_db_username";
+	private String jdbcPassword ="donation_db_password";
+	
+	public List<donation.dto.ImageDto> findAll(){
+		List<donation.dto.ImageDto> images = new ArrayList<>();
+		String sql = "SELECT image_name, image_url FROM images";
+		
+		try(Connection conn = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery()){
+			
+			while(rs.next()) {
+				ImageDto image = new ImageDto();
+				image.setImageName(rs.getString("image_name"));
+				image.setImageUrl(rs.getString("image_url"));
+				images.add(image);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return images;
+	}
+			
 }
